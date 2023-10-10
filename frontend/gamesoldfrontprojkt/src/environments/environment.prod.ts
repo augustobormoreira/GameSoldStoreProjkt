b5e637b0cbacb13c636392a995cb1fcdd832ea50
gustim <augustobormoreira@gmail.com>
2023-10-10 09:30:12 -0300

    final change made to pom.xml and application.properties

--- frontend/gamesoldfrontprojkt/src/environments/environment.prod.ts
+++ frontend/gamesoldfrontprojkt/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   // API_URL: 'http://gamestoreproject.sa-east-1.elasticbeanstalk.com'
   production: true,
-  API_URL: 'http://localhost:8080'
+  API_URL: 'http://https://gamesoldprojkt-api-5e855d6b6c8e.herokuapp.com/'
 };
